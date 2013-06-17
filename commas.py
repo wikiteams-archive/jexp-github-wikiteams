8334641d3e7f70dc9c384d6f96afdd4f3e480eb9
WikiTeams <info@wikiteams.pl>
2013-06-17 18:46:48 +0200

    further changes

--- commas.py
+++ commas.py
@@ -13,5 +13,12 @@ with open(sys.argv[1],'rb') as source:
 	    i += 1
             #wtr.writerow( '' + i + ' ' + row )
 	    row.insert(0, i)
+	    #print row
+	    if (i > 1):
+	        lista = str(row).split('/')
+	        #print lista[0]
+	        #print lista[1]
+	        row.insert(len(row)-1, lista[3])
+	        row.insert(len(row)-1, str(lista[4]).split('\'')[0])
             wtr.writerow( row )
 	    #wtr.writerow(row)
